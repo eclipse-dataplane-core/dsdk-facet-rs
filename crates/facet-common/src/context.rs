bf42f736d159fec89d4a1bf9cb74956302db96c2
Jim Marino <jim.marino@gmail.com>
2026-01-10 23:01:13 +0100

    feat: add participant context

---
+++ crates/facet-common/src/context.rs
@@ -0,0 +1,20 @@
+//  Copyright (c) 2026 Metaform Systems, Inc
+//
+//  This program and the accompanying materials are made available under the
+//  terms of the Apache License, Version 2.0 which is available at
+//  https://www.apache.org/licenses/LICENSE-2.0
+//
+//  SPDX-License-Identifier: Apache-2.0
+//
+//  Contributors:
+//       Metaform Systems, Inc. - initial API and implementation
+//
+
+use bon::Builder;
+
+/// Participant data
+#[derive(Builder, Clone)]
+pub struct ParticipantContext {
+    identifier: String,
+    audience: String,
+}
